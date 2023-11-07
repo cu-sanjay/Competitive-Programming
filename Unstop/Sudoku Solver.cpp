a57cce205fe48b5b2ce2c0e5e07fa2f172413927
Sanjay Choudhary <96792511+cu-sanjay@users.noreply.github.com>
2023-11-07 23:14:32 +0530

    Create Sudoku Solver.cpp

---
+++ Unstop/Sudoku Solver.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == num || board[row][i] == num || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(vector<vector<char>>& board) {
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == '.') {
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[row][col] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    vector<vector<char>> board(9, vector<char>(9));
+
+    // Input the Sudoku board
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    if (solveSudoku(board)) {
+        // Output the solved Sudoku board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
